7b8e12a73befb1af19b3a01de3bc50b2d65024b2
aellanaga <79834916+aellanaga@users.noreply.github.com>
2021-03-28 10:39:04 +0530

    Add files via upload

---
+++ lcmm.c
@@ -0,0 +1,32 @@
+#include <stdio.h> 
+
+int gcd(int a, int b);
+int lcm(int a, int b); 
+ 
+int main()
+{
+	int num1, num2, result;
+	
+	
+    printf("Enter two numbers:\n");
+    scanf("%d%d", &num1, &num2);
+    
+    result = lcm(num1, num2); // Function call
+    printf("Lcm of %d and %d is %d.", num1, num2, result);
+    
+    return 0; 
+}
+
+// Recursive function to return gcd of a and b 
+int gcd(int a, int b) 
+{ 
+    if (a == 0)
+        return b; 
+    return gcd(b % a, a); 
+} 
+
+// Function to return LCM of two numbers 
+int lcm(int a, int b) 
+{ 
+    return (a / gcd(a, b)) * b;
+}
